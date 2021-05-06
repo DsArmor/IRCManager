a2e1af2ed62795f798b14e8d13202477dd533270
disar <disarra2002@gmail.com>
2021-05-06 15:27:16 +0300

    Завершил репозиторий без метода getCurrentTaskFromGroup

--- app/src/main/java/com/irc_corporation/ircmanager/repository/IRCRepository.java
+++ app/src/main/java/com/irc_corporation/ircmanager/repository/IRCRepository.java
@@ -6,7 +6,9 @@ import android.util.Log;
 import com.irc_corporation.ircmanager.models.Group;
 import com.irc_corporation.ircmanager.models.GroupTask;
 import com.irc_corporation.ircmanager.models.User;
+import com.irc_corporation.ircmanager.repository.JSON.AddMember;
 import com.irc_corporation.ircmanager.repository.JSON.AddTask;
+import com.irc_corporation.ircmanager.repository.JSON.Create;
 import com.irc_corporation.ircmanager.repository.JSON.Registration;
 import com.irc_corporation.ircmanager.repository.JSON.View;
 
@@ -88,22 +90,35 @@ public class IRCRepository implements Repository{
 
     @Override
     public boolean addGroup(String email, String password, Group group) {
-        return false;
+         Create jsonBody = new Create();
+         jsonBody.newGroup.name = group.getName();
+         jsonBody.admin.email = email;
+         jsonBody.admin.password = password;
+         AsyncTaskAddGroup asyncTaskAddGroup = new AsyncTaskAddGroup();
+         asyncTaskAddGroup.execute(jsonBody);
+         return true;
     }
 
     @Override
     public boolean addMember(String email, String password, Group group, User user) {
-        return false;
+        AddMember jsonBody = new AddMember();
+        jsonBody.admin.email = email;
+        jsonBody.admin.password = password;
+        jsonBody.group.name = group.getName();
+        jsonBody.newMember.email = user.getEmail();
+        AsyncTaskAddMember asyncTaskAddMember = new AsyncTaskAddMember();
+        asyncTaskAddMember.execute(jsonBody);
+        return true;
     }
 
     @Override
     public boolean addUser(String name, String email, String password) {
-        Registration reg = new Registration();
-        reg.email = email;
-        reg.fullname = name;
-        reg.password = password;
+        Registration jsonBody = new Registration();
+        jsonBody.email = email;
+        jsonBody.fullname = name;
+        jsonBody.password = password;
         AsyncTaskAddUser asyncTaskAddUser = new AsyncTaskAddUser();
-        asyncTaskAddUser.execute(reg);
+        asyncTaskAddUser.execute(jsonBody);
         return true;
     }
 
@@ -174,6 +189,48 @@ public class IRCRepository implements Repository{
             } catch (IOException e) {
                 e.printStackTrace();
                 Log.d(LOG_TAG, "Запрос на создание пользователя НЕ отправлен");
+            }
+            return null;
+        }
+    }
+
+    private class AsyncTaskAddMember extends AsyncTask<AddMember, Void, Void> {
+        @Override
+        protected Void doInBackground(AddMember... addMembers) {
+            AddMember json = addMembers[0];
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            RetrofitService service = retrofit.create(RetrofitService.class);
+            Call<Response> call = service.addMember(json);
+            try {
+                call.execute();
+                Log.d(LOG_TAG, "Запрос на добавление пользователя отправлен");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(LOG_TAG, "Запрос на добавление пользователя НЕ отправлен");
+            }
+            return null;
+        }
+    }
+
+    private class AsyncTaskAddGroup extends  AsyncTask<Create, Void, Void> {
+        @Override
+        protected Void doInBackground(Create... creates) {
+            Create json = creates[0];
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            RetrofitService service = retrofit.create(RetrofitService.class);
+            Call<Response> call = service.create(json);
+            try {
+                call.execute();
+                Log.d(LOG_TAG, "Запрос на создание группы отправлен");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(LOG_TAG, "Запрос на создание группы НЕ отправлен");
             }
             return null;
         }
