22897e02fc6d060f25919c1a7aa40a66671c849a
disar <disarra2002@gmail.com>
2021-05-08 14:20:50 +0300

    Поменял интерфейс репозитория

--- app/src/main/java/com/irc_corporation/ircmanager/repository/Repository.java
+++ app/src/main/java/com/irc_corporation/ircmanager/repository/Repository.java
@@ -7,14 +7,11 @@ import com.irc_corporation.ircmanager.models.User;
 import java.util.List;
 
 public interface Repository {
-    boolean refresh(String email, String password);
+    void refresh(String email, String password);
     List<Group> getGroups();
     List<GroupTask> getAllTasks();
-    List<GroupTask> getTaskFromGroup(Group group);
-    List<GroupTask> getCurrentTaskFromGroup(Group group, String date); //Не просроченные, date = "dd-mm-yy"
-    boolean addTask(String email, String password, Group group, GroupTask task);
-    boolean addGroup(String email, String password, Group group);
-    boolean addMember(String email, String password, Group group, User user);
-    //----------------------------------------
-    boolean addUser(String name, String email, String password);//регистрация нового пользователя
+    void addTask(String email, String password, String groupName, String newGroupTaskName, String newGroupTaskDescription, String newGroupTaskDueDate);
+    void createGroup(String email, String password, String newGroupName);
+    void addMember(String email, String password, String groupName, String newMemberEmail);
+    void createUser(String name, String email, String password);
 }
